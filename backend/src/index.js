541c25acf3ce0535be76a50eeabf6e28553213e3
shubham rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2025-01-05 22:16:02 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,14 +4,12 @@ config()
 import connectDB from "./config/db.js";
 import { app } from "./app.js";
 
-
-
 connectDB()
   .then(() => {
+     console.log("DB Connected");
     app.listen(5003, (req, res) => {
         console.log("Server running on port 5003");
       });
-    console.log("DB Connected");
     app.on("error", (error) => {
       console.error("Server error:", error);
       throw error;
