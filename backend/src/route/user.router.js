c0279db19e42c0f6bdb3c52f003378f1a5aef508
shubham rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2025-01-05 22:17:19 +0530

    Update user.router.js

--- backend/src/route/user.router.js
+++ backend/src/route/user.router.js
@@ -3,13 +3,11 @@ import { signup , login, updateUser,userProfile} from "../controller/user.contro
 import { isLoggedIn } from "../middleware/auth.middleware.js";
 const router = Router()
 
-
-
 router.route('/signup').post(signup)
 router.route('/login').post(login)
-  
   // update route
 router.route('/update').post(isLoggedIn,updateUser)
 router.route('/profile').get(isLoggedIn,userProfile)
+router.route('/chat').post(isLoggedIn,chatInstance)
 
 export default router
