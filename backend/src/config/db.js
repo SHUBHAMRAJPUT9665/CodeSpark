f63afc2a8a2f6017f8a06b5a2a293ca9bf2d5704
shubham rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2025-01-07 11:44:50 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const connectDB = async () =>{
+const connectDB = async () => {
    const connectHost =  await mongoose.connect("mongodb+srv://shubhamrajput9665:shubham123@cluster0.w4ptbgq.mongodb.net/DV-TINDER");
    console.log("DB connect to Host", connectHost.connection.host)
 }
