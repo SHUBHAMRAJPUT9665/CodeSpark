18dd01c89e5c1a5addf2893fca5ddc38cac24b8e
shubham rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2025-01-08 16:49:18 +0530

    Update user.router.js

--- backend/src/route/user.router.js
+++ backend/src/route/user.router.js
@@ -7,7 +7,9 @@ router.route('/signup').post(signup)
 router.route('/login').post(login)
   // update route
 router.route('/update').post(isLoggedIn,updateUser)
+// user prodile route
 router.route('/profile').get(isLoggedIn,userProfile)
+// chat route
 router.route('/chat').post(isLoggedIn,chatInstance)
 router.route('/delete').delete(isLooggedIn,deleteUser)
 
