df820002c024f2292e45651e8322e249ca457c15
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2025-01-21 17:24:16 +0530

    env file update

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () =>{
-   const connectHost =  await mongoose.connect("mongodb+srv://shubhamrajput9665:shubham123@cluster0.w4ptbgq.mongodb.net/DV-TINDER");
+   const connectHost =  await mongoose.connect(process.env.DB_STRING);
    console.log("DB connect to Host", connectHost.connection.host)
 }
 
